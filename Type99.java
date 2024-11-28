7ff7d77ec140c3d4732b10cabde0b9e5e5023810
Lucas Luiz Guesser <lucas.lg17@aluno.ifsc.edu.br>
2024-11-28 19:44:12 -0300

    teste da branch

--- Type99.java
+++ Type99.java
@@ -1,5 +1,6 @@
 package China;
 import robocode.*;
+import robocode.HitByBulletEvent;
 // Código padrão
 //import java.awt.Color;
 
@@ -43,8 +44,22 @@ public class Type99 extends Robot
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
+		// demonstrate feature of debugging properties on RobotDialog
+		setDebugProperty("lastHitBy", e.getName() + " with power of bullet " + e.getPower() + " at time " + getTime());
+
+		// show how to remove debugging property
+		setDebugProperty("lastScannedRobot", null);
+
+		// gebugging by painting to battle view
+		Graphics2D g = getGraphics();
+
+		g.setColor(Color.orange);
+		g.drawOval((int) (getX() - 55), (int) (getY() - 55), 110, 110);
+		g.drawOval((int) (getX() - 56), (int) (getY() - 56), 112, 112);
+		g.drawOval((int) (getX() - 59), (int) (getY() - 59), 118, 118);
+		g.drawOval((int) (getX() - 60), (int) (getY() - 60), 120, 120);
+
+		turnLeft(90 - e.getBearing());
 	}
 	
 	/**
